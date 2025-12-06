6355dcbb8d9807f390749add448d6911595ab6b9
Rawan Sameh <Rawansamehhosny1@gmail.com>
2025-12-06 23:17:51 +0200

    modified:   Assinment2/part 1.1.js

--- Assinment2/part 1.1.js
+++ Assinment2/part 1.1.js
@@ -1,6 +1,6 @@
 // 1. Write a function that logs the current file path and directory. (0.5 Grade)
 // • Output Example:{File:“/home/user/project/index.js”, Dir:“/home/user/project”}
-const path = require("path");
+// const path = require("path");
 
 // function currentFile() {
 
